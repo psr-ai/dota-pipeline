58f56ccd5e8ae04be6ff33fcfeb57f974ec3eb41
psr <hello@prabhjotrai.com>
2019-10-29 22:03:25 -0500

    Updates

---
+++ precache-manifest.bc3c25e1a57e9faa3c84526d92559bbd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "b4a62628200bdec16c49",
+    "url": "/static/css/main.c9c1fdd0.chunk.css"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f3421a39f00a645ff292",
+    "url": "/static/js/1.f3421a39.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eb6be414b75c412c13eeee2d2e8390cb",
+    "url": "/static/media/react-logo.eb6be414.png"
+  },
+  {
+    "revision": "b4a62628200bdec16c49",
+    "url": "/static/js/main.b4a62628.chunk.js"
+  },
+  {
+    "revision": "9fcf69e5359987c9b1545aae1c2c5faa",
+    "url": "/static/media/emilyz.9fcf69e5.jpg"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "f3421a39f00a645ff292",
+    "url": "/static/css/1.f236bfa2.chunk.css"
+  },
+  {
+    "revision": "3b708f3da99c8227f0b1c1b363b9bdca",
+    "url": "/index.html"
+  }
+];
